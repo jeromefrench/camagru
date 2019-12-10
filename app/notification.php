257712edf929a4f9fbde54113268ec8fac612aac
jchardin <jerome.chardin@outook.com>
2019-12-10 17:49:14 +0100

    confirmation user

--- app/notification.php
+++ app/notification.php
@@ -10,34 +10,34 @@ function notification($text){
 if (isset($answer) && isset($answer['login_dont_exit'])){
 	notification("Le login n'existe pas");
 }
-if (isset($answer) && isset($answer['login_and_passwd_doesnt_match'])){
+else if (isset($answer) && isset($answer['login_and_passwd_doesnt_match'])){
 	notification("Le login et le mot de passe ne corresponde pas");
 }
-if (isset($answer) && isset($answer['connexion_sucessfull'])){
+else if (isset($answer) && isset($answer['connexion_sucessfull'])){
 	notification("Connexion reussi");
 }
-if (isset($answer) && isset($answer['log_out_sucessfull'])){
+else if (isset($answer) && isset($answer['log_out_sucessfull'])){
 	notification("Decoonnexion reussi");
 }
-if (isset($answer) && isset($answer['restricted'])){
+else if (isset($answer) && isset($answer['restricted'])){
 	notification("Cette partie est reserve au utilisateur connecte");
 }
-if (isset($answer) && isset($answer['information_missing'])){
+else if (isset($answer) && isset($answer['information_missing'])){
 	notification("Une information est manquante");
 }
-if (isset($answer) && isset($answer['login_already_taken'])){
+else if (isset($answer) && isset($answer['login_already_taken'])){
 	notification("Ce login existe deja, veuillez en prendre un autre");
 }
-if (isset($answer) && isset($answer['confirm_email'])){
+else if (isset($answer) && isset($answer['confirm_email'])){
 	notification("Un email de confirmation viens de vous etre envoyer");
 }
-if (isset($answer) && isset($answer['two_passwd_different'])){
+else if (isset($answer) && isset($answer['two_passwd_different'])){
 	notification("Erreur : Les deux mots de passes sont different");
 }
-if (isset($answer) && isset($answer['false_link'])){
+else if (isset($answer) && isset($answer['false_link'])){
 	notification("Erreur : Votre lien n'est pas valid");
 }
-if (isset($answer) && isset($answer['passwd_change'])){
+else if (isset($answer) && isset($answer['passwd_change'])){
 	notification("Votre mot de passe a ete changer");
 }
 ?>
--- controler/confirmation_user.php
+++ controler/confirmation_user.php
@@ -1,27 +1,56 @@
 <?php
-require_once '../app/bdd_functions.php';
 
-$login = $match_route->_slug;
-$numero_unique = $match_route->_id;
-$conn = connection_bdd();
-$data = get_user_confirmation($conn, $numero_unique);
 
-if (isset($data[0])){
-	$data = $data[0];
+if($method == "GET"){
+	require_once '../app/bdd_functions.php';
+
+	$login = $match_route->_slug;
+	$numero_unique = $match_route->_id;
+
+	$conn = connection_bdd();
+	$data = get_user_confirmation($conn, $numero_unique);
+
+	if (isset($data[0])){
+		$data = $data[0];
+	}
+	else{
+		$_SESSION['answer']['false_link'] = true;
+		header('Location: '.$fullDomain);
+		exit;
+	}
+
+	if ($data === false) {
+		$_SESSION['answer']['false_link'] = true;
+		header('Location: '.$fullDomain);
+		exit;
+	} else if (isset($data['login']) && $data['login'] == $login ) {
+		$user = [];
+		$user['login'] = $data['login'];
+		$user['mail'] = $data['mail'];
+		$user['passwd'] = $data['passwd'];
+		add_new_user($conn, $user);
+		$_SESSION['logon'] = true;
+		$_SESSION['login'] = $login;
+
+
+		$_SESSION['answer']['connexion_sucessfull'] = true;
+		header('Location: '.$fullDomain);
+		exit();
+	}
+	else {
+		$_SESSION['answer']['false_link'] = true;
+		header('Location: '.$fullDomain);
+		exit;
+	}
+}
+else {
+	echo "404 error";
 }
 
-if ($data === false) {
-	header('Location: '.$fullDomain);
-} else if (isset($data['login']) && $data['login'] == $login ) {
-	$user = [];
-	$user['login'] = $data['login'];
-	$user['mail'] = $data['mail'];
-	$user['passwd'] = $data['passwd'];
-	add_new_user($conn, $user);
-	$_SESSION['logon'] = true;
-	$_SESSION['login'] = $login;
-	header('Location: '.$fullDomain);
-} else {
-	header('Location: '.$fullDomain);
-}
+
+
+
+
+
+
 ?>
