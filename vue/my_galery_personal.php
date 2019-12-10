4fc6004e571afcf3fa329c2bea9b72453a98f186
jchardin <jerome.chardin@outook.com>
2019-12-10 20:40:19 +0100

    vue my gallery personal

---
+++ vue/my_galery_personal.php
@@ -0,0 +1,24 @@
+
+
+<h1>My galery</h1>
+<div id="galery">
+
+
+<?php foreach ($photos as $photo) { ?>
+	<a href="/galery/photo/<?= $photo['id'];?>"><img src="/<?= $photo['name']?>"/></a>
+<?php } ?>
+
+</div>
+
+<div id="pagination">
+<ul>
+<?php		if (($current_page - 1) > 0) {?>
+				<li><a href="/my-galery/<?php echo $current_page - 1;?>">Precedent</li>
+<?php			}	?>
+ 	<li class="current_page"><?php echo  $current_page;?></li>
+<?php
+if (($current_page + 1) <= $nbr_page)
+	echo '<li><a href="/my-galery/'.($current_page + 1).'">Suivant</a></li>';
+?>
+</ul>
+</div>
