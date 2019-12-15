d20f6804a8fa6b796c97890fe2f4cf9a2fd0c9a5
jchardin <jerome.chardin@outook.com>
2019-12-15 21:38:57 +0100

    js check

--- vue/header.php
+++ vue/header.php
@@ -13,7 +13,7 @@
 	<nav class="navbar" role="navigation" aria-label="main navigation">
 		<div class="navbar-brand">
 			<a class="navbar-item" href="<?= $fullDomain ; ?>"><?php require $root.'/public/img/logo.html'; ?></a>
-			<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+			<a role="button" id="jerome" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
 				<span aria-hidden="true"></span>
 				<span aria-hidden="true"></span>
 				<span aria-hidden="true"></span>
@@ -55,28 +55,13 @@ function notification(text){
 	element.innerHTML = "<p>"+text+"</p>";
 	notif.appendChild(element);
 }
-</script>
 
-
-
-<script>
 document.addEventListener('DOMContentLoaded', () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
-    });
-  }
+	$target = document.getElementById("jerome");
+	$target.addEventListener('click', () => {
+		$target.classList.toggle('is-active');
+		document.getElementById("navbarBasicExample").classList.toggle('is-active');
+	});
 });
 </script>
 
